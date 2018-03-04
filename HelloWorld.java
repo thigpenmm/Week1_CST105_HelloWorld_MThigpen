1a2375b6ad8eb6a717fac26550a5f96504317ec6
thigpenmm <36967434+thigpenmm@users.noreply.github.com>
2018-03-04 16:59:12 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**Program: Hello World Program
+ *File: HelloWorld.java
+ *Summary: Prints a greeting to the console.
+ *Author: Mercedes Thigpen
+ *Date: March 4, 2018
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hi, my name is Mercedes Thigpen.");
+        System.out.println("Hello World!");
+    }
+    
+}
